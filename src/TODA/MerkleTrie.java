3bc0296c92026d529b200ebd956e3a242c9184bb
Ana <ana.kapros@yahoo.ro>
2022-02-13 15:00:56 +0000

    finish MerkleTrie v1

--- src/TODA/MerkleTrie.java
+++ src/TODA/MerkleTrie.java
@@ -4,6 +4,7 @@ public class MerkleTrie {
     static class TrieNode {
         TrieNode branch[] = new TrieNode[2];
         String prefix; // the branch prefix from its parent to the node, empty string for root
+        String value;
     }
 
     static class Pair<U, V> {
@@ -13,6 +14,10 @@ public class MerkleTrie {
             this.key = key;
             this.value = value;
         }
+    }
+
+    private static String getHash(String value) {
+        return value;
     }
 
     private static int getLCP(String a, String b) {
@@ -25,26 +30,21 @@ public class MerkleTrie {
         return max_lcp;
     }
 
-    protected static TrieNode mergeNodes(Pair<TrieNode, String> data0, Pair<TrieNode, String> data1, int lcp) {
+    protected static TrieNode mergeNodes(Pair<TrieNode, Pair<String, Integer>> data0, Pair<TrieNode, Pair<String, Integer>> data1, int lcp) {
         TrieNode par = new TrieNode();
         par.branch[0] = data0.key;
         par.branch[1] = data1.key;
-        par.branch[0].prefix = data0.value.substring(lcp, data0.value.length());
-        par.branch[1].prefix = data1.value.substring(lcp, data1.value.length());
+        par.branch[0].prefix = data0.value.key.substring(lcp, data0.value.value);
+        par.branch[1].prefix = data1.value.key.substring(lcp, data1.value.value);
+        par.value = getHash(data0.value.key + data0.key.value + data1.value.key + data1.key.value);
         return par;
     }
 
-    protected static TrieNode createMerkleTrie(List<Pair<String, String>> data) {
+    protected static TrieNode createMerkleTrie(ArrayList<Pair<String, String>> data) {
         /*
         1. Rows are retrieved sorted from file id
         2. Find two adjacent rows whose bit strings share the longest common
-           prefix, e.g. File1 and File2
-           Filei
-           Filei+1
-           Filei+2
-           lcp01 > lcp12 => overwrite Filei on Filei+1, inc i
-           lcp01 < lcp12 => overwrite Filei+1 on Filei+2, and Filei on Filei+1
-           File2, lcp02 = min(lcp01, lcp12) = lcp12 because we choose max
+           prefix
         3. Merge those two rows, resulting in a single row that associates the lesser
         of the two file ids with H(File1,Val1,File2,Val2)
         4. Repeat steps two and three until a single file-value pair remains
@@ -54,53 +54,83 @@ public class MerkleTrie {
         // TODO: forced branch at bit 1 at end of creating trie by embeding NULL in non-existing branch of root
         int npairs = data.size();
         if (npairs == 1) {
-            //TODO: create MT with single element
-            return new TrieNode();
+            int chr = 49 - data.get(0).key.charAt(0);
+            data.add(new Pair(Integer.toString(chr), null));
+            ++ npairs;
         }
 
-        Stack<Pair<TrieNode, String>> nodes = new Stack();
+        Stack<Pair<TrieNode, Pair<String, Integer>>> nodes = new Stack();
         // create leaves
         for (int i = npairs-1; i >= 0; -- i) {
             TrieNode node = new TrieNode();
             node.branch[0] = null;
             node.branch[1] = null;
-            nodes.push(new Pair(node, data.get(i).key));
+            node.value = data.get(i).value;
+            nodes.push(new Pair(node, new Pair(data.get(i).key, data.get(i).key.length())));
         }
         TrieNode root = new TrieNode();
-        int prev_lcp = getLCP(data.get(0).value, data.get(1).value);
+        int prev_lcp = getLCP(data.get(0).key, data.get(1).key);
         for (int i = 0; i < npairs - 1; ++ i) {
-            Pair<TrieNode, String> data0 = nodes.pop();
-            Pair<TrieNode, String> data1 = nodes.pop();
+            Pair<TrieNode, Pair<String, Integer>> data0 = nodes.pop();
+            Pair<TrieNode, Pair<String, Integer>> data1 = nodes.pop();
             if (i == npairs - 2) {
                 if (prev_lcp == 0) {
                     root = mergeNodes(data0, data1, prev_lcp);
                 } else {
                     TrieNode[] root_son = new TrieNode[2];
-                    root_son[data0.value.charAt(0)-'0'] = mergeNodes(data0, data1, prev_lcp);
-                    root_son['1'-data0.value.charAt(1)] = null; // check if valid null encoding wrt TODA
-                    String s0 = new String("0");
-                    String s1 = new String("1");
-                    root = mergeNodes(new Pair(root_son[0], s0), new Pair(root_son[1], s1), 0);
+                    String s0, s1;
+                    if (data0.value.key.charAt(0) == '0') {
+                        s0 = data0.value.key;
+                        s1 = new String("1");
+                    } else {
+                        s1 = data0.value.key;
+                        s0 = new String("0");
+                    }
+                    root_son[data0.value.key.charAt(0)-'0'] = mergeNodes(data0, data1, prev_lcp);
+                    root_son['1'-data0.value.key.charAt(0)] = new TrieNode(); // check if valid null encoding wrt TODA
+                    root = mergeNodes(new Pair(root_son[0], new Pair(s0, 1)), new Pair(root_son[1], new Pair(s1, 1)), 0);
                 }
                 break;
             }
-            Pair<TrieNode, String> data2 = nodes.peek();
-            int next_lcp = getLCP(data1.value, data2.value);
+            Pair<TrieNode, Pair<String, Integer>> data2 = nodes.peek();
+            int next_lcp = getLCP(data1.value.key, data2.value.key);
             if (prev_lcp > next_lcp) {
-                prev_lcp = next_lcp;
-                nodes.push(new Pair(mergeNodes(data0, data1, prev_lcp), data0.value.substring(0, prev_lcp)));
                 // update element at position i+1 to be i
+                nodes.push(new Pair(mergeNodes(data0, data1, prev_lcp), new Pair(data0.value.key, prev_lcp)));
+                // new value for prev_lcp = lcp(0, 2) = min(lcp(0, 1), lcp(1, 2)) = lcp(1, 2);
+                prev_lcp = next_lcp;
             } else {
                 nodes.pop();
-                nodes.push(new Pair(mergeNodes(data1, data2, next_lcp), data1.value.substring(0, next_lcp)));
+                nodes.push(new Pair(mergeNodes(data1, data2, next_lcp), new Pair(data1.value.key, next_lcp)));
                 nodes.push(data0);
-                // new value for prev_lcp = lcp(0, 2) = min(lcp(0, 1); lcp(1, 2)) = lcp(0, 1);
             }
         }
         return root;
     }
 
+    public static String findValueForAddress(String address, TrieNode node) {
+        int index = 0;
+        while (node != null && node.branch[0] != null && node.branch[1] != null) {
+            node = node.branch[address.charAt(index)-48];
+            index += node.prefix.length();
+        }
+
+        return node.value;
+    }
+
     public static void main(String args[]) {
-        TrieNode node = createMerkleTrie(Arrays.asList(new Pair("00", "01")));
+        ArrayList<Pair<String, String>> pairs = new ArrayList<Pair<String, String>>(Arrays.asList(
+                new Pair("0000", "V"),
+                new Pair("0001", "W"),
+                new Pair("0010", "X"),
+                new Pair("0100", "Y"),
+                new Pair("0101", "Z")));
+        TrieNode root = createMerkleTrie(pairs);
+        System.out.println(root.value);
+        for (Pair<String, String> p: pairs) {
+            System.out.print("The value from trie for address " + p.key);
+            System.out.print(" is " + findValueForAddress(p.key, root));
+            System.out.println(" and the actual value is " + p.value);
+        }
     }
 }
