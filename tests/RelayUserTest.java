1fc35bfd51de9c2465fd6d94ebe753bf05a0cbe3
ak47na <ana.kapros@yahoo.ro>
2022-03-01 20:41:49 +0000

    add multiple transactions test

--- tests/RelayUserTest.java
+++ tests/RelayUserTest.java
@@ -9,37 +9,61 @@ import src.POP.*;
 
 public class RelayUserTest {
     public static Random rand = new Random();
-    public void test() {
-        /** 
-         * step 1: create k owners, (TODO: integrate balances)
-         * for each owner create a number of assets using User.createAsset(creator_addr, addr, signature)
-         * USer.createAsset():
-         * 1. request (s(d, I_d), I) and crt cycle root for relay from the integrity provider (check user balance)
-         * 2. call Token.creatAsset() which returns the Token to the user
-         * 3. The user adds the list of files for the address 
-         * 
-         * create a set of senders and receivers, update the number of assets held by users and add/remove them from the sets accordingly
-         * generate x simultaneous random transactions (a, b) by selecting a \in senders and b \in receivers
-         * 
-         * Transaction workflow1: 
-         * 1. user creates a fileDetail for each transaction
-         * 2. user creates the fileTrie
-         * 3. user blinds the fileTrie root and requests s(b(F0), I_d) from integrity (not tested here)
-         * 4. user sends the txpx to the relay  via sendUpdate(addr, hash)
-         * 5a. user requests POP from relay requestPOP(String address, String G_k, String G_n)
-         * 5b. user receives POP from relay sendPOP(String address, String G_k, String G_n)
-         * 6. sender sends POP to receiver
-         * 7. receiver verifies POP (not properly tested here)
-         * 
-         * Transaction workflow2:
-         * 1. user requests signature on the blinded file identifier
-         * 2. user creates the fileDetail representing an asset transfer
-         * 3. user sends the file_identifier and the update to the relay
-         * 4. the user gets the POP for the file_identifier from the relay
-         * 5. the user sends the signature on the file_identifier, and the POP to receiver to verify 
-         * 6. receiver verifies that the signature matches S(file_identifer, I_d) and that the POP is valid
-         * 
-         * */
+    public static void testMutipleTransactions(int numConsumers, int numMerchants, int numTokens, int X) {
+        ArrayList<String> cycleRoots = new ArrayList<>();
+        Relay r = new Relay();
+        MerkleTrie.TrieNode genesisCycleRoot = TestUtils.createGenesisCycleTrie(r);
+        cycleRoots.add(genesisCycleRoot.value);
+        ArrayList<Owner> consumers = new ArrayList<>();
+        ArrayList<Owner> merchants = new ArrayList<>();
+        HashMap<Owner, String> ownerPubKey = new HashMap<>();
+        HashMap<Integer, ArrayList<Token>> ownerAsset = new HashMap<>();
+
+        for (int i = 0; i < numConsumers+numMerchants; ++ i) {
+            if (i < numConsumers) {
+                Owner a = new Owner(Integer.toString(i));
+                String address = TestUtils.getRandomXBitAddr(rand, MerkleTrie.ADDRESS_SIZE);
+                consumers.add(a);
+                ownerPubKey.put(a, address);
+                ownerAsset.put(i, new ArrayList<>());
+                for (int j = 0; j < numTokens; ++ j) {
+                    ownerAsset.get(i).add(a.createAsset(genesisCycleRoot.value, address, j+1, ""));
+                }
+            } else {
+                merchants.add(new Owner(Integer.toString(i)));
+            }
+        }
+
+        for (int cycle = 1; cycle <= X; ++ cycle) {
+            ArrayList<Pair<Integer, Integer>> pairs = new ArrayList<>();
+            for (int i = 0; i < 5; ++ i) {
+                int consumerIdx = rand.nextInt() % numConsumers;
+                int merchantIdx = rand.nextInt() % numMerchants;
+                Owner a = consumers.get(consumerIdx);
+                Owner b = merchants.get(merchantIdx);
+                a.transferAsset(cycleRoots.get(cycle-1), ownerPubKey.get(a), ownerAsset.get(consumerIdx).get(i), ownerPubKey.get(b));
+                pairs.add(new Pair<Integer, Integer>(consumerIdx, merchantIdx));
+                a.sendUpdates(cycleRoots.get(cycle-1), ownerPubKey.get(a));
+            }
+            MerkleTrie.TrieNode cycleRootNode = r.createCycleTrie();
+            cycleRoots.add(cycleRootNode.value); // update cycle hash
+            for (Pair<Integer, Integer> p: pairs) {
+                Owner a = consumers.get(p.key);
+                Owner b = merchants.get(p.value - numConsumers);
+                String addressA = ownerPubKey.get(p.key);
+                String addressB = ownerPubKey.get(p.value);
+                POPSlice popSlice = r.getPOPSlice(addressA, cycleRoots.get(cycle));
+                a.receivePOP(addressA, popSlice);
+                for (int i = 0; i < 5; ++ i) {
+                    Token asset = ownerAsset.get(p.key).get(i);
+                    ArrayList<POPSlice> pop = a.getPOP(cycleRoots.get(cycle), addressA, asset);
+                    if (!b.verifyPOP(pop, addressA, addressB, "")) {
+                        throw new RuntimeException("Valid POP is not correctly verified!");
+                    }
+                }
+            }
+        }
+
     }
 
     public static void testSingleTransaction(int addressSize) {
@@ -72,15 +96,15 @@ public class RelayUserTest {
         if (!b.verifyPOP(pop, addressA, destPk, signature)) {
             throw new RuntimeException("Valid POP is not correctly verified!");
         }
-        // if (!b.verifyPOP(popSlices2, addressA, destPk, signature)) {
-        //     throw new RuntimeException("Valid POP is not correctly verified!");
-        // }
-        // if (b.verifyPOP(popSlices3, addressA, destPk, signature)) {
-        //     throw new RuntimeException("Invalid POP is considered valid!");
-        // }
+        b.receiveAsset(pop, addressA, destPk, signature, asset);
+        ArrayList<Token> assetsB = b.assets.get(destPk);
+        if (assetsB == null || !assetsB.get(assetsB.size()-1).getFileId().equals(asset.getFileId())) {
+            throw new RuntimeException("Asset not correctly redeemed!");
+        }
     }
 
     public static void main(String[] args) {
         testSingleTransaction(MerkleTrie.ADDRESS_SIZE);
+        testMutipleTransactions(4, 2, 10, 3);
     }
 }
