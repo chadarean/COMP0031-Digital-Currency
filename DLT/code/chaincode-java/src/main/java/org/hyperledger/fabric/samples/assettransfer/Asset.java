481c1b0816b4c4dec4168bb0757c5bcef56eca13
mk7a <git@mk7.dev>
2022-03-06 12:38:38 +0000

    Cycle root store contract v1

---
+++ DLT/code/chaincode-java/src/main/java/org/hyperledger/fabric/samples/assettransfer/Asset.java
@@ -0,0 +1,64 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.hyperledger.fabric.samples.assettransfer;
+
+import java.util.Objects;
+
+import org.hyperledger.fabric.contract.annotation.DataType;
+import org.hyperledger.fabric.contract.annotation.Property;
+
+import com.owlike.genson.annotation.JsonProperty;
+
+@DataType()
+public final class Asset {
+
+    @Property()
+    private final String cycleRootID;
+
+    @Property()
+    private final String cycleRootHash;
+
+    public String getCycleRootID() {
+        return cycleRootID;
+    }
+
+    public String getCycleRootHash() {
+        return cycleRootHash;
+    }
+
+    public Asset(@JsonProperty("getCycleRootID") final String cycleRootID,
+                 @JsonProperty("cycleRootHash") final String cycleRootHash) {
+        this.cycleRootID = cycleRootID;
+        this.cycleRootHash = cycleRootHash;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if ((obj == null) || (getClass() != obj.getClass())) {
+            return false;
+        }
+
+        Asset other = (Asset) obj;
+
+        return Objects.deepEquals(
+                new String[] {getCycleRootID(), getCycleRootHash()},
+                new String[] {other.getCycleRootID(), other.getCycleRootHash()});
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCycleRootID(), getCycleRootHash());
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName() + "@" + Integer.toHexString(hashCode()) + " [cycleRootID=" + cycleRootID
+                + ", cycleRootHash=" + cycleRootHash + "]";
+    }
+}
---
+++ DLT/code/chaincode-java/src/main/java/org/hyperledger/fabric/samples/assettransfer/AssetFunctions.java
@@ -0,0 +1,150 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.hyperledger.fabric.samples.assettransfer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import org.hyperledger.fabric.contract.Context;
+import org.hyperledger.fabric.contract.ContractInterface;
+import org.hyperledger.fabric.contract.annotation.Contract;
+import org.hyperledger.fabric.contract.annotation.Default;
+import org.hyperledger.fabric.contract.annotation.Info;
+import org.hyperledger.fabric.contract.annotation.Transaction;
+import org.hyperledger.fabric.shim.ChaincodeException;
+import org.hyperledger.fabric.shim.ChaincodeStub;
+import org.hyperledger.fabric.shim.ledger.KeyValue;
+import org.hyperledger.fabric.shim.ledger.QueryResultsIterator;
+
+import com.owlike.genson.Genson;
+
+@Contract(
+        name = "basic",
+        info = @Info(
+                title = "Asset Functions",
+                description = "Creates assets",
+                version = "0.0.1-SNAPSHOT"
+                ))
+@Default
+public final class AssetFunctions implements ContractInterface {
+
+    private final Genson genson = new Genson();
+
+    private enum AssetTransferErrors {
+        ASSET_NOT_FOUND,
+        ASSET_ALREADY_EXISTS
+    }
+
+    /**
+     * Creates some initial assets on the ledger.
+     *
+     * @param ctx the transaction context
+     */
+    @Transaction(intent = Transaction.TYPE.SUBMIT)
+    public void InitLedger(final Context ctx) {
+        ChaincodeStub stub = ctx.getStub();
+
+        CreateAsset(ctx, "root1", "fakeHash1");
+        CreateAsset(ctx, "root2", "fakeHash2");
+        CreateAsset(ctx, "root3", "fakeHash3");
+        CreateAsset(ctx, "root4", "fakeHash4");
+
+    }
+
+    /**
+     * Creates a new asset on the ledger.
+     *
+     * @param cycleRootID
+     * @param cycleRootHash
+     * @return the created asset
+     */
+    @Transaction(intent = Transaction.TYPE.SUBMIT)
+    public Asset CreateAsset(final Context ctx, final String cycleRootID, final String cycleRootHash) {
+        ChaincodeStub stub = ctx.getStub();
+
+        if (AssetExists(ctx, cycleRootID)) {
+            String errorMessage = String.format("Asset %s already exists", cycleRootID);
+            System.out.println(errorMessage);
+            throw new ChaincodeException(errorMessage, AssetTransferErrors.ASSET_ALREADY_EXISTS.toString());
+        }
+
+        Asset asset = new Asset(cycleRootID, cycleRootHash);
+        //Use Genson to convert the Asset into string, sort it alphabetically and serialize it into a json string
+        String sortedJson = genson.serialize(asset);
+        stub.putStringState(cycleRootID, sortedJson);
+
+        return asset;
+    }
+
+    /**
+     * Retrieves an asset with the specified ID from the ledger.
+     *
+     * @param ctx the transaction context
+     * @param cycleRootID
+     * @param cycleRootHash
+     * @return the asset found on the ledger if there was one
+     */
+    @Transaction(intent = Transaction.TYPE.EVALUATE)
+    public Asset ReadAsset(final Context ctx, final String cycleRootID, final String cycleRootHash) {
+        ChaincodeStub stub = ctx.getStub();
+        String assetJSON = stub.getStringState(cycleRootID);
+
+        if (assetJSON == null || assetJSON.isEmpty()) {
+            String errorMessage = String.format("Asset %s does not exist", cycleRootID);
+            System.out.println(errorMessage);
+            throw new ChaincodeException(errorMessage, AssetTransferErrors.ASSET_NOT_FOUND.toString());
+        }
+
+        Asset asset = genson.deserialize(assetJSON, Asset.class);
+        return asset;
+    }
+
+
+    /**
+     * Checks the existence of the asset on the ledger
+     *
+     * @param ctx the transaction context
+     * @param assetID the ID of the asset
+     * @return boolean indicating the existence of the asset
+     */
+    @Transaction(intent = Transaction.TYPE.EVALUATE)
+    public boolean AssetExists(final Context ctx, final String assetID) {
+        ChaincodeStub stub = ctx.getStub();
+        String assetJSON = stub.getStringState(assetID);
+
+        return (assetJSON != null && !assetJSON.isEmpty());
+    }
+
+
+    /**
+     * Retrieves all assets from the ledger.
+     *
+     * @param ctx the transaction context
+     * @return array of assets found on the ledger
+     */
+    @Transaction(intent = Transaction.TYPE.EVALUATE)
+    public String GetAllAssets(final Context ctx) {
+        ChaincodeStub stub = ctx.getStub();
+
+        List<Asset> queryResults = new ArrayList<>();
+
+        // To retrieve all assets from the ledger use getStateByRange with empty startKey & endKey.
+        // Giving empty startKey & endKey is interpreted as all the keys from beginning to end.
+        // As another example, if you use startKey = 'asset0', endKey = 'asset9' ,
+        // then getStateByRange will retrieve asset with keys between asset0 (inclusive) and asset9 (exclusive) in lexical order.
+        QueryResultsIterator<KeyValue> results = stub.getStateByRange("", "");
+
+        for (KeyValue result: results) {
+            Asset asset = genson.deserialize(result.getStringValue(), Asset.class);
+            System.out.println(asset);
+            queryResults.add(asset);
+        }
+
+        final String response = genson.serialize(queryResults);
+
+        return response;
+    }
+}
